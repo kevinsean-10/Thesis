ff56eaf99131868a855c373f681ce70930a60fdb
Kevin Sean Hans Lopulalan <kevinivec@gmail.com>
2024-03-14 20:41:50 +0700

    Create OOP for Spiropt

--- Spiral Optimization/new/Spiropt.py
+++ Spiral Optimization/new/Spiropt.py
@@ -1,37 +1,43 @@
 import numpy as np
-import sobol_seq
-import matplotlib.pyplot as plt
+from scipy.stats import qmc
 
 """GENERATE POINTS USING SOBOL SEQUENCE"""
-def generate_points(
-                    dim: int,
-                    npoint:int,
-                    low=-10,
-                    high=10,
-                    sobol = True):
+def generate_points(dim: int, npoint: int, low=-10, high=10, sobol=True, seed=None):
+    """
+            Generates points within the specified bounds.
+
+        Args:
+            dim: Number of dimensions.
+            npoint: Number of points to generate.
+            low: Lower bound for each variable (scalar or list/numpy array).
+            high: Upper bound for each variable (scalar or list/numpy array).
+            sobol: Flag indicating whether to use Sobol sequence (True) or random sampling (False).
+
+        Returns:
+            A numpy array of size (npoint, dim) representing the generated points.
+    """
+
     if type(low) != type(high):
         raise TypeError('The type of "low" and "high" should be the same.')
+
+    # Handle boundaries
     if type(low) == int:
-        boundaries = [(low,high) for _ in range (dim)]
-    elif type(low) == list or type(low) == np.ndarray:
+        boundaries = [(low, high) for _ in range(dim)]
+    elif type(low) in (list, np.ndarray):
         if len(low) != len(high):
             raise TypeError('The length of "low" and "high" should be the same.')
         else:
-            boundaries = [(low[i],high[i]) for i in range (len(low))]
+            boundaries = [(low[i], high[i]) for i in range(len(low))]
 
-    if sobol == True:
-        # Generate Sobol sequence points
-        sobol_points = sobol_seq.i4_sobol_generate(dim, npoint)
-        # Scale the Sobol points to fit within the specified boundaries
-        scaled_points = []
-        for i in range(dim):
-            a, b = boundaries[i]
-            scaled_dim = a + sobol_points[:, i] * (b - a)
-            scaled_points.append(scaled_dim)
-        # Transpose the scaled points to get points per dimension
-        scaled_points = np.array(list(map(list, zip(*scaled_points))))
-    
+    # Generate points based on the sobol flag
+    if sobol:
+        sampler = qmc.Sobol(d=dim,scramble=True,seed=seed)
+        sample = sampler.random(n=npoint)
+        scaled_points = qmc.scale(sample=sample,l_bounds=low,u_bounds=high)
+
     else:
+        # Generate random points
+        np.random.seed(seed)
         scaled_points = np.zeros((npoint, dim))
         for i in range(dim):
             min_val, max_val = boundaries[i]
@@ -50,8 +56,8 @@ def generate_Rij(i,j,dim,theta):
 def generate_Rn(dim,theta):
     Rn = np.eye(dim)
     for i in range(0,dim):
+        product = np.eye(dim)
         for j in range (0,i+1):
-            product = np.eye(dim)
             product *= generate_Rij(dim-i-1,dim+1-j-1,dim,theta)
         Rn *= product
     return Rn
@@ -77,13 +83,13 @@ def iter_error(old_set_of_points,new_set_of_points):
             err = diff
     return err
 
-def spiral_opt(objective_function,boundaries,theta,radius,max_iter,max_error):
+def spiral_opt(objective_function,boundaries,n_point,theta,radius,max_iter,max_error,seed):
     dim = boundaries.shape[0]
     Rn = generate_Rn(dim,theta)
     Sn = radius*Rn
     iter = 0
     iter_points = {}
-    iter_points[iter] = generate_points(dim,10,boundaries[:,0],boundaries[:,1],sobol=True)
+    iter_points[iter] = generate_points(dim,n_point,boundaries[:,0],boundaries[:,1],sobol=True,seed=seed)
     while iter <= max_iter :
         iter_points[iter+1] = update_point(iter_points[iter],objective_function,Sn)
         error = iter_error(iter_points[iter],iter_points[iter+1])
